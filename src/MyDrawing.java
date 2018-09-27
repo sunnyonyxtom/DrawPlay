e4de415c85c100319136410e5e970d04f5abdc54
David Florness <davidflorness@mines.edu>
2018-09-27 15:14:15 -0600

    Put frame in center of screen

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -28,6 +28,8 @@ public class MyDrawing extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Set the size (otherwise only title bar displays)
 		frame.setSize(400, 400);
+		// Put frame in center of screen
+		frame.setLocationRelativeTo(null);
 		
 		// Frame will not display until you set visible true
 		frame.setVisible(true);
