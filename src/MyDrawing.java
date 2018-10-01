a301fcac3aa6aca7643fca7709a2ca706a5e92e1
agoldenlasher <agoldenlasher@mymail.mines.edu>
2018-10-01 17:05:50 -0600

    Added two more cats, changed backgorund color, added title

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -1,21 +1,33 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class MyDrawing extends JPanel {
 	private Cat cat;
+	private JLabel text;
 	public MyDrawing() 
 	{
 		// Add critters to the drawing
 		cat = new Cat();
+		text = new JLabel("Kitty family");
+		text.setHorizontalAlignment(JLabel.CENTER);
+		this.add(text);
+		
+		
 	}
 	// paintComponent is called automatically when the frame needs
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 50, 50);
 		cat.draw(g, 50, 250);
+		cat.draw(g, 250, 250);
+		cat.draw(g, 250, 50);
+		
+		
 	}
 	
 	/**
@@ -25,6 +37,8 @@ public class MyDrawing extends JPanel {
 		JFrame frame = new JFrame();
 		// Program will end when the window is closed
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Change background color to blue
+		frame.setBackground(Color.CYAN);
 		// Set the size (otherwise only title bar displays)
 		frame.setSize(640, 480);
 		// Put frame in center of screen
